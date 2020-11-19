fb1ac21a1e25b583633c7a22b318358e99cf28c2
MessiAdiga <adiga1993@gmail.com>
2020-11-19 17:20:15 -0500

    Adding main file

---
+++ AhoCorasickDoubleArrayTrie-master/src/main/java/com/hankcs/algorithm/AhoCorasickDoubleArrayTrieMain.java
@@ -0,0 +1,39 @@
+package com.hankcs.algorithm;
+
+import java.util.List;
+import java.util.TreeMap;
+
+public class AhoCorasickDoubleArrayTrieMain {
+
+	public static void main(String[] args) {
+		
+		// Collect test data set
+        TreeMap<String, String> map = new TreeMap<String, String>();
+        String[] keyArray = new String[]
+                {
+                        "hers",
+                        "his",
+                        "she",
+                        "he"
+                };
+        for (String key : keyArray)
+        {
+            map.put(key, key);
+        }
+        // Build an AhoCorasickDoubleArrayTrie
+        AhoCorasickDoubleArrayTrie<String> acdat = new AhoCorasickDoubleArrayTrie<String>();
+        acdat.build(map);
+        // Test it
+        final String text = "uhers";
+        acdat.parseText(text, new AhoCorasickDoubleArrayTrie.IHit<String>()
+        {
+            @Override
+            public void hit(int begin, int end, String value)
+            {
+                System.out.printf("[%d:%d]=%s\n", begin, end, value);
+            }
+        });
+
+	}
+
+}
